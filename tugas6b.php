2f54102779ed8277f2d16cef940f6a7ba8a08cbb
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-10-08 11:00:34 +0700

    Add files via upload

---
+++ tugas6b.php
@@ -0,0 +1,5 @@
+<?php
+echo "Harga Jambu = Rp. ".$hjambu=15000; "/kg";
+echo "<br>Harga Kardus = Rp. ".$hdus=2000; "/Dus";
+echo "<br>Total Penjualan (Dus dan Jambu)= Rp. ".$total=($hjambu*5*6)+($hdus*6);
+?>
